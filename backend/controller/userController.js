dcf58748ce7c2c6282e2f92bfa33d65317d28ad1
Muhammad Haris <sheikharis574@gmail.com>
2026-09-02 13:52:44 +0500

    Create check if no user is registered

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,11 +1,12 @@
 import User from "../model/User.js";
 
-
 // Get all Users
 export const getAllUser = async (req, res) => {
   try {
     const Users = await User.find();
-
+    if (Users.length === 0) {
+      res.status(200).json({ message: "No User Registered", Users });
+    }
     res.status(200).json({ message: "All Users Fetched", Users });
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
