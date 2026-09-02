1094b7bcf2616bf902eebd3e026ffaae6de93291
Muhammad Haris <sheikharis574@gmail.com>
2026-09-02 16:16:52 +0500

    Create Route and Controller for User login with Id

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { signup, login } from "../controller/authController.js";
+import {
+  signup,
+  login,
+  loginSpecificUser,
+} from "../controller/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/login/:id", loginSpecificUser);
 
 export default router;
--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -59,3 +59,38 @@ export const login = async (req, res) => {
     res.status(500).json({ message: "Server error", error });
   }
 };
+
+export const loginSpecificUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { password } = req.body;
+    const user = await User.findOne({ _id: id });
+    if (!password) {
+      return res.status(400).json({ message: "Enter Password" });
+    }
+
+    if (!user) {
+      return res.status(400).json({ message: "User not registered" });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "secretkey", { expiresIn: "5h" });
+
+    res.status(200).json({
+      message: "Login Successfully",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
