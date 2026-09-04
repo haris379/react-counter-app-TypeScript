ac4ab38a2806b8626775c34502303e3a1eb7833f
Muhammad Haris <sheikharis574@gmail.com>
2026-09-04 14:08:58 +0500

    Create Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../model/User.js";
+
+const authMiddle = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "No token Provided",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRE_KEY);
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+};
+
+export default authMiddle;
